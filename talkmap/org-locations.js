bfc35cde23e6388820f7dc2f9f34234b04c4e72d
HoYoung1 <kimo722504@gmail.com>
2022-05-29 21:54:31 +0800

    wechat index post 추가

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,5 +1,10 @@
 var addressPoints = [
   [
+    "Xinwu District, Wuxi, Jiangsu, China",
+    31.5417862,
+    120.3617469
+  ],
+  [
     "Gangcheng District, Jinan, Shandong, China",
     36.0597837,
     117.8053635
